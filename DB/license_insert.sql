23126a0f04c6e95872a870293eaeff46a0dc5220
junoaplus <junehoo4248@naver.com>
2025-02-25 21:36:10 +0900

    insert문 추가

---
+++ DB/license_insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO license (is_license) VALUES (0);
+INSERT INTO license (is_license) VALUES (1);
---
+++ DB/vehicle_insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO vehicle (vehicle_name, need_license) VALUES
+('Car', 1),
+('Motorcycle', 1),
+('Bicycle', 0),
+('Electric Scooter', 0),
+('Truck', 1),
+('Bus', 1),
+('Tractor', 1),
+('Quad Bike', 1);
