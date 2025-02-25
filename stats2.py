2a927268ae46df92f3ae7e6b5c65d57516c171b6
kim-hansol314 <maestro4123@gmail.com>
2025-02-25 20:25:39 +0900

    Update stats2.py

--- stats2.py
+++ stats2.py
@@ -1,1 +1,16 @@
 import streamlit as st
+from streamlit_app import chart_df
+import plotly.express as px
+
+
+chart_df['density_ratio'] = chart_df['density'] / chart_df['car_amount']
+
+
+fig = px.pie(chart_df, names='city_name', values='density_ratio', title="전국 인구 밀도 대비 자동차 등록 현황")
+
+fig.update_layout(
+    width=800,  
+    height=800,  
+)
+
+st.plotly_chart(fig)
